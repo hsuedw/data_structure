214298c15fd164b0b8c513f2a590e5111e6e7a6a
edward.hc.hsu <hsuedw@gmail.com>
2020-05-03 08:42:43 -0400

    implement following member functions for vector constructors, Size(), Resize(), operator[]() and PushBack().

--- cpp/vector/main.cpp
+++ cpp/vector/main.cpp
@@ -10,11 +10,49 @@ int main(int argc, char **argv)
     a.PushBack(3.3);
     a.PushBack(4.4);
     a.PushBack(5.5);
-    for (int i = 0; i < a.Size(); ++i) {
-        std::cout << a[i] << " ";
+    a.PushBack(6.6);
+    a.PushBack(7.7);
+    a.PushBack(8.8);
+    a.PushBack(9.9);
+    a.PushBack(10.10);
+    a.PushBack(11.11);
+    std::cout << a.Size() << std::endl;
+    for (size_t i = 0; i < a.Size(); ++i) {
+        std::cout << i << ", " << a[i] << std::endl;
     }
     std::cout << std::endl << "-------------------" << std::endl;
-  
+
+    a.Resize(0);
+    std::cout << a.Size() << std::endl;
+    for (size_t i = 0; i < a.Size(); ++i) {
+        std::cout << i << ", " << a[i] << std::endl;
+    }
+    std::cout << std::endl << "-------------------" << std::endl;
+
+    double x = 123.456;
+    double y = 223.456;
+    a.PushBack(x);
+    a.PushBack(y);
+    std::cout << a.Size() << std::endl;
+    for (size_t i = 0; i < a.Size(); ++i) {
+        std::cout << i << ", " << a[i] << std::endl;
+    }
+    std::cout << std::endl << "-------------------" << std::endl;
+
+    Vector<int> b = {1, 2, 3, 4, 5};
+    std::cout << b.Size() << std::endl;
+    for (size_t i = 0; i < b.Size(); ++i) {
+        std::cout << i << ", " << b[i] << std::endl;
+    }
+    std::cout << std::endl << "-------------------" << std::endl;
+
+    Vector<int> c = b;
+     std::cout << c.Size() << std::endl;
+    for (size_t i = 0; i < c.Size(); ++i) {
+        std::cout << i << ", " << c[i] << std::endl;
+    }
+    std::cout << std::endl << "-------------------" << std::endl;
+
 
     return 0;
 }
--- cpp/vector/vector.hpp
+++ cpp/vector/vector.hpp
@@ -2,7 +2,10 @@
 #ifndef _VECTOR_HPP_
 #define _VECTOR_HPP_
 
+#include <utility>
 #include <initializer_list>
+
+#define DEFAULT_VECTOR_CAPACITY (8)
 
 template <typename T>
 class Vector
@@ -10,51 +13,81 @@ class Vector
 public:
     Vector();
     Vector(size_t n);
-    Vector(const Vector<T>& other);
-    Vector(const std::initializer_list<T>& other);
+    Vector(const Vector<T> &other);
+    Vector(const std::initializer_list<T> &other);
+    ~Vector();
 
 
     size_t Size() const;
+    void Resize(size_t n);
 
     T& operator[](size_t n);
     const T& operator[](size_t n) const;
 
-    void PushBack(const T& val);
-    void PushBack(T&& val);
+    void PushBack(const T &val);
+    void PushBack(T &&val);
 
 private:
     T* data_;
     size_t size_;
+    size_t capacity_;
 };
 
 template <typename T>
 Vector<T>::Vector()
 {
     size_ = 0;
-    data_ = nullptr;
+    capacity_ = DEFAULT_VECTOR_CAPACITY;
+    data_ = new T[capacity_];
 }
 
 template <typename T>
 Vector<T>::Vector(size_t n)
 {
     size_ = n;
-    data_ = new T[size_];
+    if (0 == n) {
+        capacity_ = DEFAULT_VECTOR_CAPACITY;
+    }
+    data_ = new T[capacity_];
 }
 
 template <typename T>
 Vector<T>::Vector(const Vector<T>& other)
 {
-
+    size_ = other.size_;
+    capacity_ = other.capacity_;
+    data_ = new T[capacity_];
+    for (int i = 0; i < size_; ++i) {
+        data_[i] = other[i];
+    }
 }
-
 
 template <typename T>
 Vector<T>::Vector(const std::initializer_list<T>& other)
 {
+    size_ = other.size();
+    if (0 == size_) {
+        capacity_ = DEFAULT_VECTOR_CAPACITY;
+        data_ = new T[capacity_];
+        return;
+    }
+    capacity_ = size_ * 2;
+    data_ = new T[capacity_];
 
+    size_t i = 0;
+    for (const T& v: other) {
+        data_[i] = v;
+        ++i;
+    }
 }
 
-
+template <typename T>
+Vector<T>::~Vector()
+{
+    if (nullptr != data_) {
+        delete[] data_;
+    }
+}
 
 template <typename T>
 size_t Vector<T>::Size() const
@@ -63,27 +96,57 @@ size_t Vector<T>::Size() const
 }
 
 template <typename T>
+void Vector<T>::Resize(size_t n)
+{
+    if (n > capacity_) {
+        capacity_ *= 2;
+        T* tempData = new T[capacity_];
+        for (int i = 0; i <= size_; ++i) {
+            tempData[i] = data_[i];
+        }
+        delete data_;
+        data_ = tempData;
+    }
+
+    if (n > size_) {
+        for (int i = size_; i < n; ++i) {
+            data_[i] = 0;
+        }
+    }
+
+    size_ = n;
+}
+
+template <typename T>
 T& Vector<T>::operator[](size_t n)
 {
-    return data_[0];
+    return data_[n];
 }
 
 template <typename T>
 const T& Vector<T>::operator[](size_t n) const
 {
-    return data_[0];
+    return data_[n];
 }
 
 template <typename T>
-void Vector<T>::PushBack(const T& val)
+void Vector<T>::PushBack(const T &val)
 {
-
+    if (size_ >= capacity_) {
+        Resize(size_);
+    }
+    data_[size_] = val;
+    ++size_;
 }
 
 template <typename T>
-void Vector<T>::PushBack(T&& val)
+void Vector<T>::PushBack(T &&val)
 {
-
+    if (size_ >= capacity_) {
+        Resize(size_);
+    }
+    data_[size_] = std::move(val);
+    ++size_;
 }
 
 #endif // _VECTOR_HPP_
