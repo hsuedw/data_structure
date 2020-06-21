37a9de7a8899e80c60d5f040ba5aa383d0830f46
hsuedw <hsuedw@gmail.com>
2020-06-21 19:08:14 +0800

    implement List class and following member functions 1. default constructor 2. destructor 3. PushFront(), PopFront() and Empty()

---
+++ cpp/list/list.hpp
@@ -0,0 +1,108 @@
+#ifndef _LIST_HPP_
+#define _LIST_HPP_
+
+#include <utility>
+#include <initializer_list>
+
+#include <iostream>
+
+template <typename T>
+class List
+{
+public:
+    List();
+    ~List();
+
+    bool Empty() const;
+    void PushFront(const T& val);
+    void PushFront(T&& val);
+    void PopFront();
+
+    void PrintList() {
+       for (ListNode_ *it = head_; it != nullptr; it = it->next) {
+           std::cout << it->data << " ";
+       } 
+       std::cout << std::endl;
+    }
+
+private:
+    struct ListNode_
+    {
+        T data;
+	ListNode_ *next;
+	ListNode_ *prev;
+	ListNode_(const T& data) : data(data), next(nullptr), prev(nullptr) {}
+	ListNode_(T&& data) : next(nullptr), prev(nullptr) {
+            std::swap(this->data, data);
+	}
+    };
+
+    ListNode_ *head_;
+    ListNode_ *tail_;
+};
+
+template <typename T>
+List<T>::List() : head_(nullptr), tail_(nullptr)
+{
+}
+
+template <typename T>
+List<T>::~List()
+{
+    while (!Empty()) {
+       PopFront(); 
+    }
+}
+
+
+template <typename T>
+bool List<T>::Empty() const
+{
+    return nullptr == head_;
+}
+
+template <typename T>
+void List<T>::PushFront(const T& val)
+{
+    ListNode_ *tmp = head_;
+    head_ = new ListNode_(val);
+    head_->next = tmp;
+    if (nullptr != tmp) {
+        tmp->prev = head_;
+    }
+    if (nullptr == tail_) {
+        tail_ = head_;
+    }
+}
+
+template <typename T>
+void List<T>::PushFront(T&& val)
+{
+    ListNode_ *tmp = head_;
+    head_ = new ListNode_(std::move(val));
+    head_->next = tmp;
+    if (nullptr != tmp) {
+        tmp->prev = head_;
+    }
+    if (nullptr == tail_) {
+        tail_ = head_;
+    }
+}
+
+template <typename T>
+void List<T>::PopFront()
+{
+    ListNode_ *tmp = head_;
+    if (Empty()) {
+        return;
+    }
+    head_ = head_->next;
+    if (nullptr != head_) {
+        head_->prev = nullptr;
+    } else {
+        tail_ = nullptr;
+    }
+    delete tmp;
+}
+
+#endif //_LIST_HPP_
---
+++ cpp/list/test_case/test_default_constructor.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "list.hpp"
+
+int main(int argc, char **argv)
+{
+    List<double> a;
+    a.PushFront(1.1);
+    a.PushFront(1.2);
+    a.PushFront(1.3);
+    a.PushFront(1.4);
+    a.PushFront(1.5);
+    a.PrintList();
+    std::cout << "-------------" << std::endl;
+
+    List<double> b;
+    double value = 2.1;
+    b.PushFront(value);
+    value = 2.2;
+    b.PushFront(value);
+    value = 2.3;
+    b.PushFront(value);
+    value = 2.4;
+    b.PushFront(value);
+    value = 2.5;
+    b.PushFront(value);
+    value = 2.6;
+    b.PushFront(value);
+    b.PrintList();
+    std::cout << "-------------" << std::endl;
+
+    return 0;
+}
